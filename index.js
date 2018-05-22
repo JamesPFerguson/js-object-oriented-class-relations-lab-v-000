53469b7685fa8fc084616a20d829470d08acada7
James Ferguson <ncjamesferguson@gmail.com>
2018-05-22 17:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,18 +29,18 @@ class Passenger {
     this.id = passengerId++;
     store.passengers.push(this);
   }
-
+  
   trips() {
-   return store.trips.filter(trip => {
-     return trip.passengerId == this.id;
-   });
- }
- drivers() {
-   return this.trips().map(trip => {
-     return trip.driver();
-   });
- }
-
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
+  
 }
 
 class Trip {
@@ -48,15 +48,15 @@ class Trip {
     this.id = tripId++;
     store.trips.push(this)
   }
-
+  
   driver() {
-   return store.drivers.find(driver => {
-     return driver.id === this.driverId;
-   });
- }
- passenger() {
-   return store.passengers.find(passenger => {
-     return passenger.id === this.passengerId;
-   });
-}
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
