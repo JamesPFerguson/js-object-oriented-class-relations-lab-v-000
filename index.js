dbeacc93688a2a6d10c0c2861fc5674ff54c2009
James Ferguson <ncjamesferguson@gmail.com>
2018-05-22 17:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+
 class Driver {
   constructor(id, name){
     this.id = id;
     this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
 }
 
